b88ad6f15f94a75eb2f96e57ff8d4fce23b4e91b
nebstarmalala <nebstartrizzle@gmail.com>
2020-09-08 20:01:00 +0300

    add func to fetch sys info

--- client.py
+++ client.py
@@ -1,14 +1,36 @@
 import os
 import socket
 import subprocess
+import psutil
+import platform
+import json
 
 PORT = 5050
 # HOST = '192.168.43.33'
 HOST = socket.gethostbyname(socket.gethostname())
 ADDR = (HOST, PORT)
+FORMAT = 'utf-8'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+def sys_info():
+
+    uname = platform.uname()
+
+    sys_info = {
+        'System': uname.system,
+        'Node Name': uname.node,
+        'Release': uname.release,
+        'Version': uname.version,
+        'Machine': uname.machine,
+        'Processor': uname.processor
+    }
+
+    sysData = json.dumps(sys_info).encode(FORMAT)
+    client.send(sysData)
+
+sys_info()
 
 while True:
     data = client.recv(2048)
